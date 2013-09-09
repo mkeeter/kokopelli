085e3a8173efd5f84768bfbbd7eacf51f1580622
Matt Keeter <matt.j.keeter@gmail.com>
2013-09-09 13:54:37 -0400

    Upgraded install_wxpython to 2.9.5

--- util/install_wxpython2.9.sh
+++ util/install_wxpython2.9.sh
@@ -1,29 +1,29 @@
+#!/bin/sh
+
+set -e
+
 echo "Using apt-get to install necessary packages"
-yes | sudo apt-get install dpkg-dev build-essential swig python2.7-dev libwebkitgtk-dev libjpeg-dev libtiff-dev checkinstall ubuntu-restricted-extras freeglut3 freeglut3-dev libgtk2.0-dev  libsdl1.2-dev libgstreamer-plugins-base0.10-dev 
+yes | sudo apt-get install libgtk2.0-dev freeglut3-dev libsdl1.2-dev libgstreamer-plugins-base0.10-dev libwebkitgtk-dev
 
 cd ~
-echo "Downloading wxPython 2.9.4.0"
-wget "http://downloads.sourceforge.net/project/wxpython/wxPython/2.9.4.0/wxPython-src-2.9.4.0.tar.bz2"
+echo "Downloading wxPython 2.9.5.0"
+wget "http://downloads.sourceforge.net/wxpython/wxPython-src-2.9.5.0.tar.bz2"
+tar xvjf wxPython-src-2.9.5.0.tar.bz2
 
-echo "Downloading wxPython 2.9.4.1 patch"
-wget "http://downloads.sourceforge.net/project/wxpython/wxPython/2.9.4.0/wxPython-src-2.9.4.1.patch"
-echo "Unzipping wxPython 2.9.4.0"
-tar xvjf wxPython-src-2.9.4.0.tar.bz2
+cd wxPython-src-2.9.5.0
+echo "Patching Python error in build-wxwidgets.py"
+sed -i 's/extra=None/extra=\[\]/' build/tools/build-wxwidgets.py
 
-echo "Applying wxPython 2.9.4.1 patch"
-patch -p 0 -d wxPython-src-2.9.4.0/ < wxPython-src-2.9.4.1.patch
-cd wxPython-src-2.9.4.0/wxPython
-
-echo "Compiling wxPython 2.9.4.1"
+cd wxPython
+echo "Compiling wxPython 2.9.5.0"
 sudo python build-wxpython.py --build_dir=../bld --install
 echo "Updating library cache"
 sudo ldconfig
 
 echo "Cleaning up"
 cd ~
-rm wxPython-src-2.9.4.0.tar.bz2
-rm wxPython-src-2.9.4.1.patch
-sudo rm -rf wxPython-src-2.9.4.0
+rm wxPython-src-2.9.5.0.tar.bz2
+sudo rm -rf wxPython-src-2.9.5.0
 
 echo "Testing:"
 python -c "import wx; print 'wx version =', wx.version()"
